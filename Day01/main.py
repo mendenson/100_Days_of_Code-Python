0b2dad2ac32952321fc7b27b556a15433ec4ca8c
Mendenson Mota <mendenson@gmail.com>
2022-01-07 14:52:31 -0700

    Day01 complete

---
+++ Day01/main.py
@@ -0,0 +1,17 @@
+# 1. Create a greeting for yout program
+
+print("#############################\nWelcome to the band ganerator\n#############################\n")
+print("I will help you to create an original name for your band be famous!\nOnly you need to do is ask my preview questions about you.\nAfter this, the magic will happening!\n")
+print("So, let's start!")
+
+# 2. Ask the user for the city that they grew up in
+
+city_name = input("What city you grew up in?\n")
+
+# 3. Ask the user for the name of a pet
+
+pet_name = input("What is your pet name?\n")
+
+# 4. Combine the name of their and pet and then their band name.
+
+print("The perfect name for your band is " + city_name + " " + pet_name + "!")
